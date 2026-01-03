3f9529503ede9a25432a8e8a8469225c8eed8557
unknown <jkinuthia412@gmail.com>
2026-01-03 22:16:27 +0300

    Fix Withdraw.css import path for Vercel build

--- src/pages/Withdraw.jsx
+++ src/pages/Withdraw.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import "../styles/withdraw.css";
+import "../styles/Withdraw.css";
 
 export default function Withdraw() {
   const { state } = useLocation();
