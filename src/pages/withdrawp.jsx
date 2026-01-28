d3632bb5652598818cd88ba608c7d2ce775401f4
html <147909829+kishcod@users.noreply.github.com>
2026-01-28 21:08:15 +0300

    Update withdrawp.jsx

--- src/pages/withdrawp.jsx
+++ src/pages/withdrawp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/withdrawp.css";
 import API from "../api"; // your backend base URL
